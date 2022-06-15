799cf8e4383ff1e7e772450a264f05c2c9d91201
anton0xf <anton0xf@gmail.com>
2022-06-16 00:46:57 +0300

    1.03: kth - version with accumulator

--- 1_lists/03_kth.pl
+++ 1_lists/03_kth.pl
@@ -1,15 +1,19 @@
 % 1.03 (*) Find the K'th element of a list.
 % The first element in the list is number 1.
 
-element_at(X, [X | _], 1) :- !.
-element_at(X, [_ | L], N)
-:- integer(N), N > 0,
-   N1 is N - 1,
-   element_at(X, L, N1), !.
+element_at(X, L, N) :- element_at_(X, L, 1, N).
+
+element_at_(X, [X | _], N, N) :- !.
+element_at_(X, [_ | L], N1, N)
+:- N2 is N1 + 1,
+   element_at_(X, L, N2, N), !.
 
 % Example:
 %?- element_at(X, [a, b, c, d, e], 3).
 %@ X = c.
+
+%?- element_at(b, [a, b, c, b, e], N).
+%@ N = 2.
 
 :- begin_tests(element_at).
 test(empty, [fail]) :- element_at(_, [], _).
@@ -36,9 +40,11 @@ test(second_from_four) :- element_at(a, [_, a, _, _], 2).
 test(third_from_four) :- element_at(a, [_, _, a, _], 3).
 test(fourth_from_four) :- element_at(a, [_, _, _, a], 4).
 test(fourth_from_four, [fail]) :- element_at(_, [_, _, _, _], 5).
+
+test(search) :- element_at(b, [a, b, c, d, e], N), N = 2.
 :- end_tests(element_at).
 
 %?- run_tests.
-%@ % PL-Unit: element_at .................... done
-%@ % All 20 tests passed
+%@ % PL-Unit: element_at ..................... done
+%@ % All 21 tests passed
 %@ true.
