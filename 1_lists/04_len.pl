3cd96ad3daa171383d9593367873d89659dea2d5
anton0xf <anton0xf@gmail.com>
2022-06-16 00:53:07 +0300

    1.04: len

---
+++ 1_lists/04_len.pl
@@ -0,0 +1,26 @@
+% 1.04 (*) Find the number of elements of a list.
+
+len(L, N) :- len_(L, 0, N).
+len_([], N, N) :- !.
+len_([_ | L], N1, N) :- N2 is N1 + 1, len_(L, N2, N), !.
+
+%?- len([_, _, _, _, _], N).
+%@ N = 5.
+
+%?- len(L, 3).
+%@ L = [_1872, _1878, _1884].
+
+:- begin_tests(len).
+test(empty) :- len([], 0).
+test(one) :- len([_], 1).
+test(two) :- len([_, _], 2).
+test(three) :- len([_, _, _], 3).
+test(four) :- len([_, _, _, _], 4).
+
+test(get_three) :- len([_, _, _], N), N = 3.
+:- end_tests(len).
+
+%?- run_tests.
+%@ % PL-Unit: len ...... done
+%@ % All 6 tests passed
+%@ true.
