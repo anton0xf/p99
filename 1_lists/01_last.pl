09b6105c576dc62f4519a741d30cf7eb4e5e7b55
anton0xf <anton0xf@gmail.com>
2022-06-16 15:41:23 +0300

    1.01: last - tests without choice points

--- 1_lists/01_last.pl
+++ 1_lists/01_last.pl
@@ -1,7 +1,19 @@
 % 1.01 (*) Find the last element of a list.
-my_last(Last, [Last]).
+my_last(Last, [Last]) :- !.
 my_last(Last, [_ | Tail]) :- my_last(Last, Tail), !.
 
 %?- my_last(X, [a, b, c, d]).
 %@ X = d.
 
+:- begin_tests(my_last).
+test(empty, [fail]) :- my_last(_, []).
+test(one) :- my_last(a, [a]).
+test(two) :- my_last(b, [a, b]).
+test(two_wrong, [fail]) :- my_last(a, [a, b]).
+test(three) :- my_last(c, [a, b, c]).
+:- end_tests(my_last).
+
+%?- run_tests.
+%@ % PL-Unit: my_last ..... done
+%@ % All 5 tests passed
+%@ true.
