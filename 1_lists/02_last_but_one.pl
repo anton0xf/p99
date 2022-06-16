7145dbb20857d0a1025973af0af023a607bbaedf
anton0xf <anton0xf@gmail.com>
2022-06-16 16:28:22 +0300

    1.02: last but one - get rid of cuts

--- 1_lists/02_last_but_one.pl
+++ 1_lists/02_last_but_one.pl
@@ -1,6 +1,11 @@
 % 1.02 (*) Find the last but one element of a list.
-last_but_one([X, _], X) :- !.
-last_but_one([_, X, Y | R], Z) :- last_but_one([X, Y | R], Z), !.
+last_but_one([Y1, Y2 | L], X) :- last_but_one_(L, Y1, Y2, X).
+last_but_one_([], X, _, X).
+last_but_one_([Y3 | L], Y1, Y2, X) :- last_but_one_(L, Y2, Y3, X).
+
+%?- trace.
+%?- last_but_one([1, 2], X).
+%@ X = 1.
 
 %?- last_but_one([1, 2, 3, 4], X).
 %@ X = 3.
