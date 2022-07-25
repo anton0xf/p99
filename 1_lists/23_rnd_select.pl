d24b21a2b968e6e0f2071be1b573549c0e7c728f
anton0xf <anton0xf@gmail.com>
2022-07-25 23:52:48 +0300

    1.23: use random_between/3

--- 1_lists/23_rnd_select.pl
+++ 1_lists/23_rnd_select.pl
@@ -10,18 +10,19 @@ rnd_select(_, 0, []) :- !.
 rnd_select(L, N, [X | R]) :-
     N > 0, N1 is N - 1,
     length(L, M),
-    M1 is M + 1,
-    random(1, M1, K),
+    random_between(1, M, K),
     remove_at(X, L, K, L1),
     rnd_select(L1, N1, R).
 
 %% Example:
 %% ?- rnd_select([a, b, c, d, e, f, g, h], 3, L).
-%@ L = [f, h, a].
-%@ L = [h, c, e].
-%@ L = [g, d, a].
+%@ L = [d, h, a].
+%@ L = [f, a, g].
+%@ L = [c, b, h].
+%@ L = [c, h, b].
 
 %% ?- rnd_select([a, b, c], 3, L).
-%@ L = [c, b, a].
 %@ L = [b, c, a].
 %@ L = [a, c, b].
+%@ L = [c, a, b].
+
