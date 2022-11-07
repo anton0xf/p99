d51e4c2b4dfa5772b7a1bdf22208c53588252391
anton0xf <anton0xf@gmail.com>
2022-11-08 02:28:40 +0300

    1.27 group: generic group/3

--- 1_lists/27_group.pl
+++ 1_lists/27_group.pl
@@ -7,14 +7,23 @@
 
 %% Let's start from group2(L, G1, G2) which group 4 elements into
 %% 2 disjoint groups by 20 elements
-group2_([], 0, [], 0, []).
-group2_([X | L], N1, [X | G1], N2, G2) :-
-    M is N1 - 1,
-    group2_(L, M, G1, N2, G2).
-group2_([X | L], N1, G1, N2, [X | G2]) :-
-    M is N2 - 1,
-    group2_(L, N1, G1, M, G2).
-group2(L, G1, G2) :- group2_(L, 2, G1, 2, G2).
+group2_([], 0, [], []).
+group2_([X | L], N, [X | G1], G2) :-
+    N > 0, N1 is N - 1,
+    group2_(L, N1, G1, G2).
+group2_([X | L], N, G1, [X | G2]) :- group2_(L, N, G1, G2).
+group2(L, G1, G2) :- group2_(L, 2, G1, G2).
+
+%?- group2_([a], 0, X, Y).
+%@ X = [],
+%@ Y = [a].
+
+%?- group2([a, b, c], G1, G2).
+%@ G1 = [a, b], %@ G2 = [c] ;
+%@ G1 = [a, c], %@ G2 = [b] ;
+%@ G1 = [b, c], %@ G2 = [a] ;
+%@ false.
+
 %?- group2([a, b, c, d], G1, G2).
 %@ G1 = [a, b], %@ G2 = [c, d] ;
 %@ G1 = [a, c], %@ G2 = [b, d] ;
@@ -50,6 +59,23 @@ group3(L, G1, G2, G3) :- group3_(L, 2, G1, 3, G2, 4, G3).
 
 %% b) Generalize the above predicate in a way that we can specify
 %%    a list of group sizes and the predicate will return a list of groups.
+
+group([], [], []).
+group(L, [N | Ns], [G | Gs]) :- group2_(L, N, G, R), group(R, Ns, Gs).
+
+%?- group([a, b, c, d], [2, 2], Gs).
+%@ Gs = [[a, b], [c, d]] ;
+%@ Gs = [[a, c], [b, d]] ;
+%@ Gs = [[a, d], [b, c]] ;
+%@ Gs = [[b, c], [a, d]] ;
+%@ Gs = [[b, d], [a, c]] ;
+%@ Gs = [[c, d], [a, b]] ;
+%@ false.
+
+%?- aggregate_all(count,
+%   group([aldo, beat, carla, david, evi, flip, gary, hugo, ida], [2, 3, 4], Gs),
+%   Count).
+%@ Count = 1260.
 
 %% Example:
 %% ?- group([aldo, beat, carla, david, evi, flip, gary, hugo, ida], [2, 2, 5], Gs).
